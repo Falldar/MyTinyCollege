734077a576c92271368fc747acaee39ce78b7f69
Daniel <Daniel@ADVP-DBUJOLD>
2016-04-25 10:55:48 -0300

    added school context db

---
+++ MyTinyCollege/DAL/SchoolContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyTinyCollege.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MyTinyCollege.DAL
+{
+    class SchoolContext:DbContext
+    {
+        //Constructor: Initialize connectionstring (to match web.config)
+        public SchoolContext() : base("DefaultConnection")
+        {
+
+        }
+
+        //Specify Entity Sets - Corresponding to Database tables
+        //Each single entity corresponds to a row in a table
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        //public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        //public DbSet<Department> Departments { get; set; }
+
+        //Specifying singular table names
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            /* Using Fluent API
+             * Take care of many-to-many relationsihp between instructor and course entities. EF Code First can configure
+             * this for us, but if we don't override the names we will get mapping such as 
+             * InstructorInstructorID for the InstructorID column
+             */
+            modelBuilder.Entity<Course>()
+               .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+               .Map(t => t.MapLeftKey("CourseID").MapRightKey("InstructorID").ToTable("CourseInstructor"));
+
+            //The above code will create a junction (bridging) table called "CourseInstructor
+            //with 2 FK columns CourseID, and InstructorID
+            //CourseID -> Course
+            //InstructorID -> Instructor
+            //and a composite PK on CourseID + InstructorID
+        }
+
+    }
+}
